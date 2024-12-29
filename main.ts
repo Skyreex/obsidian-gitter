55ed4d605d7163155bf4bce9a526b748b7cfaac3
skyreex <akramfarikh3@gmail.com>
2024-12-29 23:18:05 +0100

    initial commit

---
+++ main.ts
@@ -0,0 +1,163 @@
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+
+interface GitterSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: GitterSettings = {
+	mySetting: "default",
+};
+
+export default class Gitter extends Plugin {
+	settings: GitterSettings;
+
+	async onload() {
+		await this.loadSettings();
+		this.initRubbon();
+
+		this.addCommand({
+			id: "open-sample-modal-simple",
+			name: "Open sample modal (simple)",
+			allback: () => {
+				new SampleModal(this.app).open();
+			},
+		});
+		// This adds an editor command that can perform some operation on the current editor instance
+		this.addCommand({
+			id: "sample-editor-command",
+			name: "Sample editor command",
+			ditorCallback: (editor: Editor, view: MarkdownView) => {
+				console.log(editor.getSelection());
+				editor.replaceSelection("Sample Editor Command");
+			},
+		});
+		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		this.addCommand({
+			id: "open-sample-modal-complex",
+			name: "Open sample modal (complex)",
+			heckCallback: (checking: boolean) => {
+				// Conditions to check
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (markdownView) {
+					// If checking is true, we're simply "checking" if the command can be run.
+					// If checking is false, then we want to actually perform the operation.
+					if (!checking) {
+						new SampleModal(this.app).open();
+					}
+
+					// This command will only show up in Command Palette when the check function returns true
+					return true;
+				}
+			},
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
+	}
+
+	onunload() {
+		console.log("onunload plugin");
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	initRubbon() {
+		const pushIcon = this.addRibbonIcon(
+			"cloud-upload",
+			"Sync",
+			(evt: MouseEvent) => this.pushAction(evt)
+		);
+
+		const pulIcon = this.addRibbonIcon(
+			"cloud-download",
+			"Pull",
+			(evt: MouseEvent) => this.pullAction(evt)
+		);
+		// ribbonIconEl.addClass("my-plugin-ribbon-class");
+	}
+
+	pushAction(evt: MouseEvent) {
+		console.log("localStorage", localStorage);
+		Notice("This is a push notice!");
+	}
+
+	pullAction(evt: MouseEvent) {
+		console.log("localStorage", localStorage);
+		Notice("This is a push notice!");
+	}
+}
+
+class SampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: Gitter;
+
+	constructor(app: App, plugin: Gitter) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
